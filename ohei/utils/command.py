b6963a3ba17e4b63e2f0b93c216fd16c7b23be94
revol.cai <revol.cai@daocloud.io>
2016-07-17 04:18:40 +0800

    fix exec_cmd
    Signed-off-by: revol.cai <revol.cai@daocloud.io>

--- ohei/utils/command.py
+++ ohei/utils/command.py
@@ -6,7 +6,8 @@ from subprocess import Popen, PIPE
 def exec_cmd(cmd):
     if isinstance(cmd, str):
         cmd = shlex.split(cmd)
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    sh = True if len(cmd) == 1 else False
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=sh)
     out, err = proc.communicate()
     if proc.returncode == 0:
         return str(out).strip(), str(err).strip()
@@ -16,5 +17,6 @@ def exec_cmd(cmd):
 
 if __name__ == '__main__':
     pprint(exec_cmd('ls'))
-    pprint(exec_cmd(['/bin/ls', '--aaa']))
     pprint(exec_cmd('python --version'))
+    pprint(exec_cmd(['/bin/ls', '--aaa']))
+    pprint(exec_cmd('dmidecode'))
